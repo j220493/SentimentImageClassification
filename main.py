9c11088036a974a265db371db9f0f9f105ac0ea9
Jorge Martelo <j220493@gmail.com>
2023-04-11 12:00:11 -0500

    se optimiza modelo para clasificacion

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from zipfile import ZipFile
 # import cv2
 import imghdr
+import keras
 from PIL import Image
 import matplotlib.pyplot as plt
 import shutil
@@ -10,7 +11,6 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
-from tensorflow.math import confusion_matrix
 import numpy as np
 
 # Extracting dataset (only once)
@@ -18,7 +18,7 @@ with ZipFile('happySad.zip', 'r') as zip:
     zip.extractall()
     print('done')
 
-# removing weird images
+# removing weird images (only once)
 ext = ['jpeg', 'jpg', 'bmp', 'png']
 imageClasses = ['happy', 'sad']
 for folder in imageClasses:
@@ -34,26 +34,11 @@ for folder in imageClasses:
             print('Issue with image {}'.format(imagePath))
 
 # Counting final images
-len(os.listdir(os.path.join('happySad', 'happy')))
-len(os.listdir(os.path.join('happySad', 'sad')))
-
-# Creating final folder and moving into new folder (Only once)
-os.mkdir('smallData')
-os.mkdir(os.path.join('smallData', 'happy'))
-os.mkdir(os.path.join('smallData', 'sad'))
-
-for image in os.listdir('happy'):
-    source = os.path.join('happy', image)
-    destination = os.path.join('smallData', 'happy', image)
-    shutil.copyfile(source, destination)
-
-for image in os.listdir('sad'):
-    source = os.path.join('sad', image)
-    destination = os.path.join('smallData', 'sad', image)
-    shutil.copyfile(source, destination)
+print(len(os.listdir(os.path.join('happySad', 'happy'))))
+print(len(os.listdir(os.path.join('happySad', 'sad'))))
 
 # Creating pipline
-data = tf.keras.utils.image_dataset_from_directory('happySad')
+data = tf.keras.utils.image_dataset_from_directory('happySad', batch_size=20, image_size=(256, 256))
 data.class_names
 
 # Image iterator
@@ -63,12 +48,11 @@ batch[0].shape
 
 # Plotting set of images
 batch = dataIterator.next()
-fig, ax = plt.subplots(ncols=4, figsize=(20, 20))
+fig, ax = plt.subplots(ncols=4, figsize=(10, 10))
 for idx, img in enumerate(batch[0][:4]):
     ax[idx].imshow(img.astype(int))
     ax[idx].title.set_text(batch[1][idx])
 
-# Adding scaling step into the pipline
 data = data.map(lambda x, y: (x / 255, y))
 print(data.as_numpy_iterator().next()[0].min(),
       '\n', data.as_numpy_iterator().next()[0].max())
@@ -88,7 +72,11 @@ print("Number of batches for train:", len(train))
 
 # Model architecture
 model = Sequential()
-model.add(Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
+model.add(tf.keras.layers.RandomFlip('horizontal'))
+# model.add(tf.keras.layers.RandomZoom(0.2))
+# model.add(tf.keras.layers.RandomRotation(0.2))
+model.add(tf.keras.layers.Rescaling(1 / 255, offset=-1))
+model.add(Conv2D(16, (3, 3), 1, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Conv2D(32, (3, 3), 1, activation='relu'))
 model.add(MaxPooling2D())
@@ -97,16 +85,18 @@ model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+model.build(input_shape=(None, 256, 256, 3))
 model.summary()
 
 # Compiling model
-model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss=tf.losses.BinaryCrossentropy(),
+              metrics=['accuracy'])
 
 # Creating log file
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir='log')
 
 # Training model
-hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+hist = model.fit(train, epochs=45, validation_data=val, callbacks=[tensorboard_callback])
 
 # Evaluating training performance
 fig = plt.figure()
@@ -136,14 +126,13 @@ for batch in val.as_numpy_iterator():
     pre.update_state(y, yhat)  # Precision
     re.update_state(y, yhat)  # Recall
     acc.update_state(y, yhat)
-    cm = confusion_matrix(y, yhat)
 
 # Evaluating test
 testBatches = test.as_numpy_iterator().next()
 print(testBatches[0].shape)
 fig, ax = plt.subplots(ncols=4, figsize=(10, 10))
 for idx, img in enumerate(testBatches[0][:4]):
-    ax[idx].imshow(img.astype(float))
+    ax[idx].imshow(img.astype(int))
     ax[idx].title.set_text(testBatches[1][idx])
 
 for batch in test.as_numpy_iterator():
@@ -158,7 +147,7 @@ print("Precision:", pre.result().numpy(),
       "\nAccuracy:", acc.result().numpy())
 
 # Evaluating new data
-newTest = Image.open('esposa.jpg')
+newTest = Image.open('jorgeFeliz2.jpg')
 newTest = newTest.rotate(270)
 imgplot = plt.imshow(newTest)
 plt.show()
@@ -169,5 +158,5 @@ imgplot = plt.imshow((resized / 255))
 plt.show()
 
 # Predicting
-yhat = model.predict(np.expand_dims((resized / 255), 0))
+yhat = model.predict(np.expand_dims(resized, 0))
 print(yhat)
